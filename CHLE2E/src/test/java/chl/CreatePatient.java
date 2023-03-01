ab5857b1d77b8df108ad9336f4736ffe8426dd66
hp <hp@Rehan>
2023-03-01 14:32:26 +0530

    First commit

---
+++ CHLE2E/src/test/java/chl/CreatePatient.java
@@ -0,0 +1,91 @@
+package chl;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import utils.ExcelDataProvider;
+
+public class CreatePatient {
+	
+	String alertId = null;
+	public static WebDriver driver=null;
+	
+	@BeforeSuite(description = "Login into CHL")
+	public void driverInitiation(){
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	
+	}
+	
+	@Test(priority = 1)
+	public void login() {
+		driver.get("https://chl.alpha-app.tls.global/Account/Login");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("Email")).sendKeys("mainsubowner@test.com");
+		driver.findElement(By.id("Password")).sendKeys("Test@123");
+		
+		
+		driver.findElement(By.xpath("//*[@id=\"loginForm\"]/form/div[3]/div/input")).click();
+		
+		
+	}
+	
+	
+	@Test(priority = 2,dataProvider = "Patient Data",dataProviderClass = ExcelDataProvider.class)
+	public void createPatient(String Subsription, String firstname, String lastname, String email
+			, String password, String contactNumber, String dob, String gender, String timeZone) {
+		
+		driver.findElement(By.xpath("//*[@id=\"logoutForm\"]/ul[1]/li[2]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"main-body\"]/p/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"ParentSubscriptionID_chosen\"]/a/span")).click();
+	
+	
+		
+		driver.findElement(By.xpath("//*[@id=\"ParentSubscriptionID_chosen\"]/div/div/input")).sendKeys(Subsription);
+		driver.findElement(By.xpath("//*[@id=\"timeZone_chosen\"]/div/div/input")).sendKeys(Keys.TAB);
+
+		driver.findElement(By.id("FirstName")).sendKeys(firstname);
+		driver.findElement(By.id("LastName")).sendKeys(lastname);
+		driver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys(email);
+		driver.findElement(By.xpath("//*[@id=\"ConfirmEmail\"]")).sendKeys(email);
+		driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys(password);
+		driver.findElement(By.xpath("//*[@id=\"ConfirmPassword\"]")).sendKeys(password);
+		driver.findElement(By.xpath("//*[@id=\"PhoneNumber\"]")).sendKeys(contactNumber);
+		driver.findElement(By.xpath("//*[@id=\"DateOfBirth\"]")).sendKeys(dob);
+		
+		WebElement gender_dropDown =  driver.findElement(By.id("Gender"));
+		Select select_gdropDown = new Select(gender_dropDown);
+		if(gender.equals("Male"))
+		{
+			
+			select_gdropDown.selectByValue("1");
+		}
+		else {select_gdropDown.selectByValue("2");}
+		
+
+		//driver.findElement(By.xpath("//*[@id=\"City\"]")).sendKeys(city);
+		//driver.findElement(By.xpath("//*[@id=\"State\"]")).sendKeys(state);
+		//driver.findElement(By.xpath("//*[@id=\"Zipcode\"]")).sendKeys(zipCode);
+		
+		
+		driver.findElement(By.xpath("//*[@id=\"timeZone_chosen\"]/a/span")).click();
+		driver.findElement(By.xpath("//*[@id=\"timeZone_chosen\"]/div/div/input")).sendKeys(timeZone);
+		driver.findElement(By.xpath("//*[@id=\"timeZone_chosen\"]/div/div/input")).sendKeys(Keys.DOWN, Keys.TAB);
+		
+	
+		
+		driver.findElement(By.xpath("//*[@id=\"addRowsSave\"]")).click();
+		
+		
+	}
+
+}
---
+++ CHLE2E/src/test/java/utils/ExcelUtils.java
@@ -0,0 +1,78 @@
+package utils;
+
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtils {
+
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+	static XSSFCell cell;
+
+
+	public ExcelUtils(String excelPath, String sheetName) {
+
+		try {
+			workbook = new XSSFWorkbook(excelPath);
+			sheet = workbook.getSheet(sheetName);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+
+	public int getColoumnCount() {
+
+
+		int columnCount = sheet.getRow(0).getPhysicalNumberOfCells();
+		//System.out.println("Number of Rows: "+	columnCount);
+		return columnCount;
+
+	} 
+	
+	public int getRowCount() {
+
+
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		//System.out.println("Number of Rows: "+	rowCount);
+		return rowCount;
+
+	} 
+
+	public String getCellStringData(int rowNum, int colNum) {
+		String stringData = sheet.getRow(rowNum).getCell(colNum).getStringCellValue();				
+		//System.out.println(stringData);
+		return stringData;
+
+
+	}
+
+	public double getCellNumericData(int rowNum, int colNum) {
+
+		double numericData = sheet.getRow(rowNum).getCell(colNum).getNumericCellValue();
+		//System.out.println(numericData);
+		return numericData;
+
+
+	} 	
+	
+	public CellType getCellType(int rowNum, int colNum) {
+		
+		CellType type = sheet.getRow(rowNum).getCell(colNum).getCellType();
+		return type;
+
+		
+	
+
+
+	} 	
+
+
+}
+
