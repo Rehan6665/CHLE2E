0ce7e52aeb886d1731890240134000e758702e38
hp <hp@Rehan>
2023-03-06 15:42:12 +0530

    API's for panic, motion and doorWindow created

--- CHLE2E/src/test/java/utils/ApiClass.java
+++ CHLE2E/src/test/java/utils/ApiClass.java
@@ -3,6 +3,8 @@ package utils;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.meta.When;
+
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
@@ -29,14 +31,64 @@ public class ApiClass {
 		jsonData.put("timestampStr", "");
 		jsonData.put("eventId", "");
 		jsonData.put("deviceId", "NDagency11P2");
-		jsonData.put("battery", "72");
-		
+		jsonData.put("battery", "72"); 	
+
 		RestAssured.baseURI = "https://liberate-staging.servicebus.windows.net/sensor-events/messages";
 		RestAssured.given().header("Content-Type","application/json");
 		RestAssured.given().header("Authorization","SharedAccessSignature sr=liberate-staging.servicebus.windows.net&sig=ZpcQn1DG8ruQXcVP0fgqa5kP6j6XR9czXSCBkLlYuic%3d&se=1761807990&skn=Hubitat")
 		.contentType(ContentType.JSON).body(jsonData.toJSONString())
 		.when().post().then().statusCode(201);
-		
-		
+
+
 	}
+
+	@Test
+	public void doorAndWindow() {
+
+
+		JSONObject jsonData = new JSONObject();
+		jsonData.put("endTimestamp", "0");
+		jsonData.put("isLearning", "false");
+		jsonData.put("isSimulated", "false");
+		jsonData.put("presenceDetected", "false");
+		jsonData.put("contact", "open");
+		jsonData.put("statusUpdateTimestamp", "0");
+		jsonData.put("type", "");
+		jsonData.put("timestampStr", "");
+		jsonData.put("eventId", "");
+		jsonData.put("deviceId", "DWagency1p1");
+		jsonData.put("battery", "72"); 
+
+		RestAssured.baseURI ="https://liberate-staging.servicebus.windows.net/sensor-events/messages";
+		RestAssured.given().header("Content-Type","application/json");
+		RestAssured.given().header("Authorization","SharedAccessSignature sr=liberate-staging.servicebus.windows.net&sig=ZpcQn1DG8ruQXcVP0fgqa5kP6j6XR9czXSCBkLlYuic%3d&se=1761807990&skn=Hubitat")
+		.contentType(ContentType.JSON).body(jsonData.toJSONString()).when().post().then().statusCode(201);
+
+	}
+
+	@Test
+	public void motion() {
+
+		JSONObject jsonData = new JSONObject();
+		jsonData.put("endTimestamp", "0");
+		jsonData.put("isLearning", "false");
+		jsonData.put("isSimulated", "false");
+		jsonData.put("motion", "inactive");
+		jsonData.put("statusUpdateTimestamp", "0");
+		jsonData.put("type", "");
+		jsonData.put("timestampStr", "");
+		jsonData.put("eventId", "");
+		jsonData.put("deviceId", "Motionagency1p1");
+		jsonData.put("battery", "72"); 
+
+		RestAssured.baseURI ="https://liberate-staging.servicebus.windows.net/sensor-events/messages";
+		RestAssured.given().header("Content-Type","application/json");
+		RestAssured.given().header("Authorization","SharedAccessSignature sr=liberate-staging.servicebus.windows.net&sig=ZpcQn1DG8ruQXcVP0fgqa5kP6j6XR9czXSCBkLlYuic%3d&se=1761807990&skn=Hubitat")
+		.contentType(ContentType.JSON).body(jsonData.toJSONString()).when().post().then().statusCode(201);
+
+
+	}
+
+
+
 }
